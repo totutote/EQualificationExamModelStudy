24137b3985513032aa60eebfa4cfc055dad2cad7
Yu Totsuka <tty102fnio@gmail.com>
2025-02-25 01:12:43 +0900

    R-CNNトレーニング関数の改善とIoU計算機能の追加

--- src/r-cnn/rcnn_train.py
+++ src/r-cnn/rcnn_train.py
@@ -1,37 +1,26 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import time
 from torchvision.datasets import VOCDetection
 from torchvision.transforms import ToTensor, Compose, Resize
 from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
 from rcnn_util import RCNN, VOCUtils
 
-# 学習の設定
-def train_rcnn(model, dataloader_train, dataloader_val, optimizer, criterion, num_epochs):
+def train_rcnn(model, dataloader_train, dataloader_val, optimizer, criterion_cls, criterion_bbox, num_epochs):
     model.train()
     for epoch in range(num_epochs):
         # tqdmでdataloaderをラップしてプログレスバーを表示
         with tqdm(total=len(dataloader_train), desc=f'Epoch {epoch+1}/{num_epochs}', unit='batch') as pbar:
             for images, targets in dataloader_train:
                 images = images.to(device)
-                rois = []
-                labels = []
-                bbox_targets = []
-                for target in targets:
-                    for object in target['annotation']['object']:
-                        bbox = object['bndbox']
-                        rois.append([0, int(bbox['xmin']), int(bbox['ymin']), int(bbox['xmax']), int(bbox['ymax'])])
-                        labels.append(int(label_mapping[object['name']]))
-                        bbox_targets.append([int(bbox['xmin']), int(bbox['ymin']), int(bbox['xmax']), int(bbox['ymax'])])
-                rois = torch.FloatTensor(rois).to(device)
-                labels = torch.tensor(labels).to(device)
-                bbox_targets = torch.FloatTensor(bbox_targets).to(device)
+                rois, labels, bbox_targets = VOCUtils.extract_roi_data(targets, label_mapping, device)
                 
                 optimizer.zero_grad()
                 class_logits, bbox_preds = model(images, rois)
-                loss_cls = criterion(class_logits, labels)
-                loss_bbox = criterion(bbox_preds, bbox_targets)
+                loss_cls = criterion_cls(class_logits, labels)
+                loss_bbox = criterion_bbox(bbox_preds, bbox_targets)
                 loss = loss_cls + loss_bbox
                 loss.backward()
                 optimizer.step()
@@ -44,55 +33,63 @@ def train_rcnn(model, dataloader_train, dataloader_val, optimizer, criterion, nu
         model.eval()
         correct = 0
         total = 0
+        total_iou = 0.0  # IoUの合計を初期化
+        num_samples = 0  # サンプル数を初期化
         with torch.no_grad():
             with tqdm(total=len(dataloader_val), desc=f'Epoch {epoch+1}/{num_epochs}', unit='batch') as pbar:
                 for images, targets in dataloader_val:
                     images = images.to(device)
-                    rois = []
-                    labels = []
-                    for target in targets:
-                        for object in target['annotation']['object']:
-                            bbox = object['bndbox']
-                            rois.append([0, int(bbox['xmin']), int(bbox['ymin']), int(bbox['xmax']), int(bbox['ymax'])])
-                            labels.append(int(label_mapping[object['name']]))
-                    rois = torch.FloatTensor(rois).to(device)
-                    labels = torch.tensor(labels).to(device)
-                    
-                    class_logits, _ = model(images, rois)
+                    rois, labels, bbox_targets = VOCUtils.extract_roi_data(targets, label_mapping, device)
+
+                    class_logits, bbox_preds = model(images, rois)
                     _, predicted = torch.max(class_logits, 1)
                     total += labels.size(0)
                     correct += (predicted == labels).sum().item()
+
+                    # IoUを計算
+                    iou = VOCUtils.calculate_iou(bbox_preds, bbox_targets)
+                    total_iou += iou.sum().item()  # バッチ内のIoUの合計を加算
+                    num_samples += len(iou)  # サンプル数を加算
             
                 accuracy = 100 * correct / total
-                pbar.set_postfix({'accuracy': accuracy})
+                mean_iou = total_iou / num_samples  # 平均IoUを計算
+                pbar.set_postfix({'accuracy': accuracy, 'mean_iou': mean_iou})  # 平均IoUをプログレスバーに表示
                 pbar.update(1)
             model.train()
 
-# Pascal VOCデータセットの読み込み
-transform = Compose([Resize((224, 224)), ToTensor()])
-voc_dataset = VOCDetection(root='dataset/', year='2012', image_set='train', download=True, transform=transform)
-
-# データセットをトレーニングと検証に分割
-train_size = int(0.8 * len(voc_dataset))
-val_size = len(voc_dataset) - train_size
-voc_train, voc_val = random_split(voc_dataset, [train_size, val_size])
-
-# データローダーの作成
-dataloader_train = DataLoader(voc_train, batch_size=32, num_workers=4, pin_memory=True, collate_fn=VOCUtils.collate_fn)
-dataloader_val = DataLoader(voc_val, batch_size=32, num_workers=4, pin_memory=True, collate_fn=VOCUtils.collate_fn)
-
-label_mapping = VOCUtils.get_voc_labels(voc_dataset)
-
-# モデルの初期化
-device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
-num_classes = len(label_mapping)
-model = RCNN(num_classes).to(device)
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-criterion = nn.CrossEntropyLoss()
 
 if __name__ == '__main__':
+    start_time_1 = time.time()
+
+    # Pascal VOCデータセットの読み込み
+    transform = Compose([Resize((448, 448)), ToTensor()])
+    voc_dataset = VOCDetection(root='dataset/', year='2012', image_set='train', download=True, transform=transform)
+
+    # データセットをトレーニングと検証に分割
+    train_size = int(0.8 * len(voc_dataset))
+    val_size = len(voc_dataset) - train_size
+    voc_train, voc_val = random_split(voc_dataset, [train_size, val_size])
+
+    # データローダーの作成
+    dataloader_train = DataLoader(voc_train, batch_size=16, num_workers=4, pin_memory=True, collate_fn=VOCUtils.collate_fn)
+    dataloader_val = DataLoader(voc_val, batch_size=16, num_workers=4, pin_memory=True, collate_fn=VOCUtils.collate_fn)
+
+    label_mapping = VOCUtils.get_voc_labels(voc_dataset)
+
+    # モデルの初期化
+    device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+    num_classes = len(label_mapping)
+    model = RCNN(num_classes).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    criterion_cls = nn.CrossEntropyLoss()
+    criterion_bbox = nn.SmoothL1Loss()
+
+    end_time_1 = time.time()
+
+    print(f"処理1にかかった時間: {end_time_1 - start_time_1:.4f}秒")
+
     # 学習の実行
-    train_rcnn(model, dataloader_train, dataloader_val, optimizer, criterion, num_epochs=3)
+    train_rcnn(model, dataloader_train, dataloader_val, optimizer, criterion_cls, criterion_bbox, num_epochs=5)
 
     # モデルの保存
     torch.save(model.state_dict(), 'rcnn_model.pth')
--- src/r-cnn/rcnn_util.py
+++ src/r-cnn/rcnn_util.py
@@ -2,18 +2,17 @@ import torch
 import torch.nn as nn
 from torchvision.models import resnet50, ResNet50_Weights
 from torchvision.ops import RoIPool
+from torchvision.ops import box_iou
 
 # R-CNNの定義
 class RCNN(nn.Module):
     def __init__(self, num_classes):
         super(RCNN, self).__init__()
         resnet = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
-        for param in resnet.parameters():
-            param.requires_grad = False  # ResNetの重みを固定
         self.backbone = nn.Sequential(*list(resnet.children())[:-2])
-        self.roi_pool = RoIPool(output_size=(7, 7), spatial_scale=1.0/16)
-        self.classifier = nn.Linear(resnet.fc.in_features * 7 * 7, num_classes)
-        self.bbox_regressor = nn.Linear(resnet.fc.in_features * 7 * 7, 4)
+        self.roi_pool = RoIPool(output_size=(4, 4), spatial_scale=1.0/32)
+        self.classifier = nn.Linear(resnet.fc.in_features * 4 * 4, num_classes)
+        self.bbox_regressor = nn.Linear(resnet.fc.in_features * 4 * 4, 4)
         
     def forward(self, images, rois):
         features = self.backbone(images)
@@ -24,19 +23,67 @@ class RCNN(nn.Module):
         return class_logits, bbox_preds
 
 class VOCUtils:
-    def __init__(self):
-        pass
 
     @staticmethod
     def collate_fn(batch):
-        images, targets = zip(*batch)
-        return torch.stack(images, 0), targets
+        images = [item[0] for item in batch]
+        targets = [item[1] for item in batch]
+        images = torch.stack(images)
+        return images, targets
 
     @staticmethod
-    def get_voc_labels(dataset):
+    def get_voc_labels(voc_dataset):
         labels = set()
-        for _, target in dataset:
-            for obj in target['annotation']['object']:
-                labels.add(obj['name'])
-        sorted_label = sorted(labels)
-        return {label: idx for idx, label in enumerate(sorted_label)}
+        for i in range(len(voc_dataset)):
+            annotation = voc_dataset[i][1]
+            for object in annotation['annotation']['object']:
+                labels.add(object['name'])
+        labels = sorted(list(labels))
+        label_mapping = {label: i for i, label in enumerate(labels)}
+        return label_mapping
+
+    @staticmethod
+    def extract_roi_data(targets, label_mapping, device, resized_width=448, resized_height=448):
+        rois = []
+        labels = []
+        bbox_targets = []
+        for batch_index, target in enumerate(targets):
+            original_width = int(target['annotation']['size']['width'])
+            original_height = int(target['annotation']['size']['height'])
+            width_ratio = resized_width / original_width
+            height_ratio = resized_height / original_height
+
+            for object in target['annotation']['object']:
+                bbox = object['bndbox']
+                x_min = int(bbox['xmin'])
+                y_min = int(bbox['ymin'])
+                x_max = int(bbox['xmax'])
+                y_max = int(bbox['ymax'])
+
+                x_min_resized = int(x_min * width_ratio)
+                y_min_resized = int(y_min * height_ratio)
+                x_max_resized = int(x_max * width_ratio)
+                y_max_resized = int(y_max * height_ratio)
+
+                rois.append([batch_index, x_min_resized, y_min_resized, x_max_resized, y_max_resized])
+                labels.append(int(label_mapping[object['name']]))
+                bbox_targets.append([x_min_resized, y_min_resized, x_max_resized, y_max_resized])
+
+        rois = torch.FloatTensor(rois).to(device)
+        labels = torch.tensor(labels).to(device)
+        bbox_targets = torch.FloatTensor(bbox_targets).to(device)
+        return rois, labels, bbox_targets
+    
+    @staticmethod
+    def calculate_iou(bbox_preds, bbox_targets):
+        # bbox_predsとbbox_targetsを[N, 4]の形式に変換
+        bbox_preds = bbox_preds.view(-1, 4)
+        bbox_targets = bbox_targets.view(-1, 4)
+        
+        # IoUを計算
+        iou = box_iou(bbox_preds, bbox_targets)
+        
+        # 各予測バウンディングボックスに対する最大のIoUを返す
+        max_iou_per_bbox = torch.max(iou, dim=1)[0]
+        
+        return max_iou_per_bbox
